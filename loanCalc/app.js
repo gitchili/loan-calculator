301722db44e12f7c7296a98e28018bea28db21f3
gitchili <jameslphillips@gmail.com>
2019-01-14 23:51:42 -0700

    JS CalculatorSetUp

--- loanCalc/app.js
+++ loanCalc/app.js
@@ -2,7 +2,7 @@
 document.getElementById('loan-form').addEventListener('submit', calculateResults);
 
 
-function calculateResults(e){
+function calculateResults(e) {
     console.log("Calculating...");
     // UX variables
     const amount = document.getElementById('amount');
@@ -12,6 +12,21 @@ function calculateResults(e){
     const totalPayment = document.getElementById('total-payment');
     const totalInterest = document.getElementById('total-interest');
 
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    } else {
+        console.log("check the numbers");
+    }
     e.preventDefault();
-    
+
 }
