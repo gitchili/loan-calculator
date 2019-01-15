bdc79c332b73ddd6f29e845e16ee5118f7323b99
gitchili <jameslphillips@gmail.com>
2019-01-14 23:30:21 -0700

    JS calculatorSetUp init

--- loanCalc/app.js
+++ loanCalc/app.js
@@ -1,5 +1,17 @@
-// calc-input
-document.getElementById('calculate-button').addEventListener('click',
-function(e){
-    console.log("wasClicked")
-});
+// listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+
+function calculateResults(e){
+    console.log("Calculating...");
+    // UX variables
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    e.preventDefault();
+    
+}
